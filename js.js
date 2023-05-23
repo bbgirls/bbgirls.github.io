6ff72cc862102fa567cfa6a5e07d81216711e239
bbgirls <134437196+bbgirls@users.noreply.github.com>
2023-05-24 01:19:34 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+function get_link(a = 1) {
+    if (a !== 1) {
+        var a = "YUhSMGNITTZMeTkzZDNjdVptRmpaV0p2YjJzdVkyOXRMM0JsY20xaGJHbHVheTV3YUhBL2MzUnZjbmxmWm1KcFpEMHhNVEV3TVRrM05qQTFNRFUwTkRNbWFXUTlNVEV3TWprMk1ERXdOVGMzT0RFNA==";
+        a = atob(a);
+    } else {
+        //real
+        var a = "YUhSMGNITTZMeTl5YVhOeGRXVm5ZV3hyZUM1amIyMHZQM1YwYlY5emIzVnlZMlU5ZVZZMWN6TnRVVlYyU2xCR1Z5WjFkRzFmWTJGdGNHRnBaMjQ5UTFWVVNVdEpSdz09=";
+        a = atob(a);
+    }
+    return a;
+}
